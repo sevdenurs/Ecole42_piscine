8047bfe08374c122c0b75c1d58992f772910ade9
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-03-10 01:01:07 +0300

    Update ft_ultimate_div_mod.c

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -21,3 +21,17 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	*a = div / mod;
 	*b = div % mod;
 }
+
+/*
+#include <stdio.h>
+int main()
+{
+	int	x;
+	int	y;
+
+	x = 15;
+	y = 3;
+	ft_ultimate_div_mod(&x, &y);
+	printf("%d | %d", x, y);
+}
+*\
