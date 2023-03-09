8c3014966c32dcc96f7edb089c315f8c1b2c6c49
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-03-10 01:07:17 +0300

    Update ft_sort_int_tab.c

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -32,3 +32,15 @@ void	ft_sort_int_tab(int *tab, int size)
 		size--;
 	}
 }
+/*
+#include <stdio.h>
+int	main()
+{
+	int tab[6] = {7, 6, 3, 4, 2, 5};
+	int size = 6;
+
+	ft_sort_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]); 
+	return (0);
+}
+*\
