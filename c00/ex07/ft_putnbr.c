eb3bef5b3848d55957371caa75cda82a129dd6e0
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-03-09 23:36:56 +0300

    Update ft_putnbr.c

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -39,3 +39,19 @@ void	ft_putnbr(int nb)
 	else
 		ft_putchar(nb + 48);
 }
+/*
+int main (void)
+{
+	ft_putnbr(1426);
+	ft_putchar('\n');
+	ft_putnbr(-2147483648);
+	ft_putchar('\n');
+	ft_putnbr(42);
+	ft_putchar('\n');
+	ft_putnbr(2147483647);
+	ft_putchar('\n');
+	ft_putnbr(2);
+	ft_putchar('\n');
+
+}
+*/
