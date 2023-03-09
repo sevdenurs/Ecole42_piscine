7502df9bec08ee875c8b99fcc65448e66bf81473
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-03-10 01:05:55 +0300

    Update ft_rev_int_tab.c

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -25,3 +25,15 @@ void	ft_rev_int_tab(int *tab, int size)
 		a++;
 	}
 }
+/*
+#include <stdio.h>
+int	main()
+{
+	int tab[6] = {0, 1, 2, 3, 4, 5};
+	int size = 6;
+
+	ft_rev_int_tab(tab, size);
+	printf("%d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]); 
+	return (0);
+}
+*\
