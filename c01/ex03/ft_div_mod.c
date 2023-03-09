6ed1e0b8929aa30892864e01f4d50bcdb26017ec
Sevde Nur TAŞKIN <119888860+sevdenurs@users.noreply.github.com>
2023-03-10 00:59:36 +0300

    Update ft_div_mod.c

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -16,3 +16,19 @@ void	ft_div_mod(int a, int b, int *div, int *mod)
 	*div = a / b;
 	*mod = a % b;
 }
+/*
+#include <stdio.h>
+int	main()
+{
+	int a;
+	int b;
+	int x;
+	int y;
+
+	a = 10;
+	b = 5;
+	ft_div_mod(a, b, div, mod);
+	printf("%d\n", x);
+	printf("%d", y);
+}
+*\
